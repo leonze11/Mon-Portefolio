c5645fdfaa83163f5a19502caf930ebba6114c45
leonze11 <43454038+leonze11@users.noreply.github.com>
2019-01-25 15:26:21 +0100

    Add files via upload

---
+++ Mon portfolio/src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //open modal
+  $("#open_modal").click(function() {
+    $("#modal_to_open").css("display", "block")
+  });
+});
+//close
+$("#close_modal").click(function() {
+    $("#modal_to_open").css("display", "none");
+})
